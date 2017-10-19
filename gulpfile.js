c09c575925518a35b78ebc0e6f9d301ac907b914
promotionbessa <bessapromotionimmobiliere@gmail.com>
2017-10-19 16:38:47 +0100

    optimised logo

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat');
-const imagemin = require('gulp-imagemin');
+var imageop = require('gulp-image-optimization');
 
 
 gulp.task('scripts', function() {
   return gulp.src(['./_js/jquery.min.js', './_js/materialize.min.js', './_js/bessa-carousel.js' , './_js/init.js','./_js/bessa.js'])
     .pipe(concat('all.js'))
     .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('images', function(cb) {
+    gulp.src(["_img/avancement/**/*.png","_img/avancement/**/*.jpg","_img/avancement/**/*.gif","_img/avancement/**/*.jpeg"]).pipe(imageop({
+        optimizationLevel: 8,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest("img/avancement")).on("end", cb).on("error", cb);
 });
