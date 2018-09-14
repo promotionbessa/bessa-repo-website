13f1ed75aecfd8dcc02da1a5b118aa2d73046c5c
samyilias <iliasaboub@gmail.com>
2018-09-14 07:07:55 +0100

    landing page setup

--- gulpfile.js
+++ gulpfile.js
@@ -12,4 +12,10 @@ gulp.task('landing', function() {
     .pipe(concat('landing.js'))
     .pipe(gulp.dest('./js/'));
 });
+
+gulp.task('landingfusion', function() {
+  return gulp.src(['./_js/landing/jquery.min.js',"./_js/landing/jquery.scrolly.min.js","./_js/landing/jquery.dropotron.min.js","./_js/landing/jquery.scrollex.min.js","./_js/landing/browser.min.js","./_js/landing/breakpoints.min.js","./_js/landing/util.js","./_js/landing/main.js"])
+    .pipe(concat('landingfusion.js'))
+    .pipe(gulp.dest('./js/'));
+});
 gulp.task('default', ['scripts']);
